3a204c0dca22efaa2c7568fea70918ae6b9365fb
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-03-02 22:18:41 -0700

    Started MakeChange project, completed US 1-3

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 
 
 Program to calculate change
+
+1. The scanner is asking me as the "seller/vendor" to enter price of the item being sold.( double because it is money)
+2. input scanner
+3. print message asking for price...(double)
+4. print the money the buyer gives you...(also double)
+5. use if else to calculate whether or not the money given was too much too little or exact using <, > , ==
+6. print appropriate messages dependent on if the money received too little or exact.
+7. if the money was too much and customer needs change....(change is going to equal cash - price)
--- src/com/skilldistillery/projectmakechange/MakeChangeApp.java
+++ src/com/skilldistillery/projectmakechange/MakeChangeApp.java
@@ -1,5 +1,25 @@
 package com.skilldistillery.projectmakechange;
 
-public class MakeChangeApp {
+import java.util.Scanner;
 
+public class MakeChangeApp {
+	public static void main(String[] args) {
+
+		Scanner kb = new Scanner(System.in);
+
+		System.out.print("Enter the price of the item:");
+		double price = kb.nextDouble();
+		System.out.println("Money recieved:");
+		double cash = kb.nextDouble();
+		if (cash < price) {
+			System.out.println("Not enough cash given");
+
+		} else if (cash == price) {
+			System.out.println("Exact cash given, no change due");
+		} else if (cash > price) {
+			double change = cash - price;
+			System.out.println("Change: " + change);
+		}
+
+	}
 }
